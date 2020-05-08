79d45d44ae7cd4558cd04ab44ab580527da4edde
Cyrus Vandrevala <cyrus@8thlight.com>
2020-05-08 14:53:21 -0500

    It actually does something now

--- Prime Factors/PrimeFactorsViewController.swift
+++ Prime Factors/PrimeFactorsViewController.swift
@@ -1,5 +1,9 @@
 import UIKit
 
 public class PrimeFactorsViewController: UIViewController {
-    
+    @IBOutlet var primeFactorsText: UITextField!
+    @IBOutlet var primeFactorsOutput: UILabel!
+    @IBAction func calculatePrimes() {
+        primeFactorsOutput.text = primeFactorsText.text
+    }
 }
