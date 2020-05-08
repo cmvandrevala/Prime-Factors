ecadb86c913f11eb1b009afaa5316891655faf71
Cyrus Vandrevala <cyrus@8thlight.com>
2020-05-08 14:02:43 -0500

    Clean up view controller

--- Prime Factors/ViewController.swift
+++ Prime Factors/ViewController.swift
@@ -1,20 +1,4 @@
-//
-//  ViewController.swift
-//  Prime Factors
-//
-//  Created by Cyrus Vandrevala on 5/8/20.
-//  Copyright © 2020 Cyrus Vandrevala. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
 }
-
