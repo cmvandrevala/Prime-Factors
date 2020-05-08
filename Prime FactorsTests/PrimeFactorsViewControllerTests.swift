52db59474901872bf6330ce59c0d38ab7e76b22e
Cyrus Vandrevala <cyrus@8thlight.com>
2020-05-08 14:37:22 -0500

    Working storyboard

---
+++ Prime FactorsTests/PrimeFactorsViewControllerTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+import Prime_Factors
+
+class PrimeFactorsViewControllerTests: XCTestCase {
+
+    func testExample() {
+        let primeFactorsViewController = PrimeFactorsViewController()
+        XCTAssertEqual(456, 456)
+    }
+}
